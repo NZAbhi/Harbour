47b4c13df7c2c50793eff0d452c424b5ae532f3b
Nuruz <nuruzzaman856@gmail.com>
2020-11-08 05:07:55 +0100

    Updated

---
+++ HarbourManagement/DisplayDockInformation.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HarbourManagement
+{
+    /// <summary>
+    /// Display all the information Of the dock
+    /// </summary>
+    class DisplayDockInformation
+    {
+        private static Dictionary<int, string> harbour = BerthManagement.harbour;
+        public static void DisplayInfo(List<Boat> _parkedBoats)
+        {
+            Console.WriteLine("Dock[N] \t BoatType \t IdentityNumber\t Weight \t MaxSpeed \t  Others \t \t \t  DaysIntheDock");
+            Console.WriteLine();
+            var boatIds = harbour.Values.Where(x => x != null).Distinct();
+            foreach (var id in boatIds)
+            {
+
+                if (id.Split(", ").Length == 2)
+                {
+                    var ids = id.Split(", ");
+                    foreach (var _id in ids)
+                    {
+                        var key = harbour.FirstOrDefault(x => x.Value == id).Key;
+                        var boat = _parkedBoats.FirstOrDefault(x => x.IdentityNumber == _id.Trim());
+                        if (boat != null)
+                        {
+                            if (boat is RowingBoat)
+                            {
+                                Console.WriteLine($"{key}    \t\t {boat.GetType().Name} -- \t {boat.IdentityNumber} -- \t {boat.Weight} kg  -- \t {boat.MaximumSpeed} km/h  -- \t MaximumNumberOfPassengers:{((RowingBoat)boat).MaximumNumberOfPassengers} -- \t {boat.DaysCount} D");
+                            }
+                        }
+                        else if (boat == null)
+                        {
+                            Console.WriteLine($"{key}    \t\t Empty-- \t --- -- \t --  -- \t --  -- \t -- -- \t ----");
+
+                        }
+                    }
+                }
+                else
+                {
+                    var keys = harbour.Where(pair => pair.Value == id).Select(pair => pair.Key);
+                    var boat = _parkedBoats.FirstOrDefault(x => x.IdentityNumber == id);
+                    if (boat != null)
+                    {
+                        if (boat is RowingBoat)
+                        {
+                            Console.WriteLine($"{string.Join("-", keys)}   \t\t {boat.GetType().Name} -- \t {boat.IdentityNumber} -- \t {boat.Weight} kg  -- \t {boat.MaximumSpeed} km/h  -- \t NumberOfHorsepower:{((RowingBoat)boat).MaximumNumberOfPassengers} hp -- \t\t {boat.DaysCount} D");
+                        }
+                        else if (boat is MotorBoat)
+                        {
+                            Console.WriteLine($"{string.Join("-", keys)}   \t\t {boat.GetType().Name} -- \t {boat.IdentityNumber} -- \t {boat.Weight} kg  -- \t {boat.MaximumSpeed} km/h  -- \t NumberOfHorsepower:{((MotorBoat)boat).NumberOfHorsepower} hp -- \t\t {boat.DaysCount} D");
+                        }
+                        else if (boat is SailBoat)
+                        {
+                            Console.WriteLine($"{string.Join("-", keys)}    \t\t {boat.GetType().Name} -- \t {boat.IdentityNumber} -- \t {boat.Weight} kg  -- \t {boat.MaximumSpeed} km/h  -- \t BoatLength:{((SailBoat)boat).BoatLength} --\t\t        {boat.DaysCount} D");
+                        }
+                        else if (boat is CargoShip)
+                        {
+                            Console.WriteLine($"{string.Join("-", keys)}  \t {boat.GetType().Name} -- \t {boat.IdentityNumber} -- \t {boat.Weight} kg  -- \t {boat.MaximumSpeed} km/h  -- \t NumberOfContainers:{((CargoShip)boat).NumberOfContainers} -- \t\t {boat.DaysCount} D");
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("....................................................................................................................................");
+
+            var TotalBoats = _parkedBoats
+                            .Where(x => x != null)
+                            .GroupBy(t => t.BoatType);
+
+            foreach (var boats in TotalBoats)
+            {
+                if (boats.Key != null)
+                {
+                    Console.Write($"{boats.Key}: {boats.Count()}\t");
+                }
+
+            }
+
+            var Weights = _parkedBoats.Sum(w => w.Weight);
+
+            Console.Write($"Total Weights:{Weights}\t");
+
+            if (_parkedBoats.Count > 0)
+            {
+                var AverageSpeed = _parkedBoats.Sum(a => a.MaximumSpeed);
+                Console.Write($"AverageSpeed:{AverageSpeed / _parkedBoats.Count}\t ");
+            }
+            Console.Write($"Total Parking: {harbour.Count}\t");
+            Console.Write($"EmptyParking: {BerthManagement.emptyParking}\t");
+            Console.Write($"RefusedBoats Today: {BerthManagement.refusedFromDock}\t");
+            Console.WriteLine();
+            Console.WriteLine("....................................................................................................................................");
+
+        }
+        public static void ParkingFullWarning() 
+        {
+            Console.WriteLine("ParkingFull Come Tomorrow Please.");
+        }
+
+
+    }
+}
---
+++ HarbourManagement/FileSystem.cs
@@ -0,0 +1,97 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace HarbourManagement
+{
+    /// <summary>
+    /// Creat File and read from the file
+    /// </summary>
+    class FileSystem
+    {
+        private static List<Boat> _parkedBoats = BerthManagement._parkedBoats;
+        private static Dictionary<int, string> harbour = BerthManagement.harbour;
+        public static void CreatSpreadsheet()
+        {
+            string spreadsheetPath = @"C:\Visual Stadio\HarbourManagement\DockedBoatsInHarbor.csv";
+            var csv = new StringBuilder();
+            for (int i = 0; i < _parkedBoats.Count; i++)
+            {
+                if (_parkedBoats[i] != null)
+                {
+
+                    if (_parkedBoats[i] is RowingBoat)
+                    {
+                       var  dockedBoat = (_parkedBoats[i].BoatType, _parkedBoats[i].IdentityNumber, _parkedBoats[i].Weight, _parkedBoats[i].MaximumSpeed, _parkedBoats[i].DaysCount, ((RowingBoat)_parkedBoats[i]).MaximumNumberOfPassengers, _parkedBoats[i].ParkingPlace);
+                        csv.AppendLine(string.Join(",", dockedBoat));
+                    }
+                    else if (_parkedBoats[i] is MotorBoat)
+                    {
+                        var dockedBoat = (_parkedBoats[i].BoatType, _parkedBoats[i].IdentityNumber, _parkedBoats[i].Weight, _parkedBoats[i].MaximumSpeed, _parkedBoats[i].DaysCount, ((MotorBoat)_parkedBoats[i]).NumberOfHorsepower, _parkedBoats[i].ParkingPlace);
+                        csv.AppendLine(string.Join(",", dockedBoat));
+                    }
+                    else if (_parkedBoats[i] is SailBoat)
+                    {
+                        var dockedBoat = (_parkedBoats[i].BoatType, _parkedBoats[i].IdentityNumber, _parkedBoats[i].Weight, _parkedBoats[i].MaximumSpeed, _parkedBoats[i].DaysCount, ((SailBoat)_parkedBoats[i]).BoatLength, _parkedBoats[i].ParkingPlace);
+                        csv.AppendLine(string.Join(",", dockedBoat));
+                    }
+                    else if (_parkedBoats[i] is CargoShip)
+                    {
+                        var dockedBoat = (_parkedBoats[i].BoatType, _parkedBoats[i].IdentityNumber, _parkedBoats[i].Weight, _parkedBoats[i].MaximumSpeed, _parkedBoats[i].DaysCount, ((CargoShip)_parkedBoats[i]).NumberOfContainers, _parkedBoats[i].ParkingPlace);
+                        csv.AppendLine(string.Join(",", dockedBoat));
+                    }
+                }
+            }
+            File.WriteAllText(spreadsheetPath, csv.ToString());
+        }
+        public static List<Boat> ReadBoatInfoFromFile()
+        {
+            var boats = new List<Boat>();
+
+            const string filePath = @"C:\Visual Stadio\HarbourManagement\DockedBoatsInHarbor.csv";
+            foreach (string boat in File.ReadLines(filePath, System.Text.Encoding.UTF7))
+            {
+                char[] delimiterChars = { ' ', ',', '(', ')' };
+                string[] boatData = boat.Trim().Split(delimiterChars);
+                if (boatData[1] == "RowingBoat")
+                {
+                    var rowingBoat = new RowingBoat(boatData[1], boatData[3],
+                        int.Parse(boatData[5]), int.Parse(boatData[7]), int.Parse(boatData[9]), int.Parse(boatData[11]));
+                    rowingBoat.ParkingPlace = boatData[13];
+                    BerthManagement.emptyParking -= .5;
+                    boats.Add(rowingBoat);
+                }
+                else if (boatData[1] == "MotorBoat")
+                {
+                    var motorBoat = new MotorBoat(boatData[1], boatData[3],
+                                            int.Parse(boatData[5]), int.Parse(boatData[7]), int.Parse(boatData[9]), int.Parse(boatData[11]));
+                    motorBoat.ParkingPlace = boatData[13];
+                    BerthManagement.emptyParking -= 1;
+                    boats.Add(motorBoat);
+                }
+                else if (boatData[1] == "SailBoat")
+                {
+                    var sailBoat = new SailBoat(boatData[1], boatData[3],
+                                            int.Parse(boatData[5]), int.Parse(boatData[7]), int.Parse(boatData[9]), int.Parse(boatData[11]));
+                    sailBoat.ParkingPlace = boatData[13];
+                    BerthManagement.emptyParking -= 2;
+                    boats.Add(sailBoat);
+                }
+                else if (boatData[1] == "CargoShip")
+                {
+                    var cargoShip = new CargoShip(boatData[1], boatData[3],
+                                            int.Parse(boatData[5]), int.Parse(boatData[7]), int.Parse(boatData[9]), int.Parse(boatData[11]));
+                    cargoShip.ParkingPlace = boatData[13];
+                    BerthManagement.emptyParking -= 4;
+                    boats.Add(cargoShip);
+                }
+            }
+            return boats;
+        }
+
+    }
+}
